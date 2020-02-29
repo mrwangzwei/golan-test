823619dbd01ef440bd3b2f9855dcad1e6e067626
wangzhiwei <942248412@qq.com>
2020-02-29 15:39:52 +0800

    mysql-test

--- app/controller/userInfo.go
+++ app/controller/userInfo.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	request_struct "self-test/app/common-data/request-struct"
@@ -17,12 +16,39 @@ func FindUserInfo(c *gin.Context) {
 		return
 	}
 
-	var userList []model.Userinfo
-
+	var err error
 	db := dao.Mysql
-	err := db.Model(&model.Userinfo{}).Find(&userList)
-	fmt.Println(err)
+	//查询列表
+	var userList []model.UserInfoModel
+	err = db.Model(&model.UserInfoModel{}).Where("name LIKE ? AND phone = ?", "%"+req.Name+"%", req.Phone).Find(&userList).Error
 
-	c.JSON(http.StatusOK, utils.Respone(1, "查询成功", userList))
+	//单个查询
+	var singleUser model.UserInfoModel
+	err = db.Model(&model.UserInfoModel{}).Where(map[string]interface{}{"name": req.Name, "phone": req.Phone}).First(&singleUser).Error
+
+	//创建新记录
+	createUser := model.UserInfoModel{
+		Name:  "ssss",
+		Phone: "sssssss",
+	}
+	trans := db.Begin() //开事务
+	err = trans.Model(&model.UserInfoModel{}).Create(&createUser).Error
+	//修改记录
+	err = trans.Model(&model.UserInfoModel{}).Where("name = ?", req.Name).Update(map[string]interface{}{
+		"name":  "qqq",
+		"phone": "11111111111",
+	}).Error
+	if (err != nil) {
+		trans.Rollback()
+	}
+	trans.Commit()
+
+	var finaluserList []model.UserInfoModel
+	err = db.Model(&model.UserInfoModel{}).Find(&finaluserList).Error
+
+	c.JSON(http.StatusOK, utils.Respone(1, "查询成功", map[string]interface{}{
+		"userlist":      userList,
+		"singleUser":    singleUser,
+		"finaluserList": finaluserList}))
 	return
 }
--- app/model/userInfo.go
+++ app/model/userInfo.go
@@ -2,13 +2,13 @@ package model
 
 import "time"
 
-type Userinfo struct {
-	ID        int64  `json:"id"`
+type UserInfoModel struct {
+	ID        uint64  `json:"id"`
 	Name      string `json:"name"`
 	Phone     string `json:"phone"`
-	CreatedAt time.Time
+	CreatedAt time.Time `json:"created_at"`
 }
 
-func (Userinfo) TableName() string {
+func (UserInfoModel) TableName() string {
 	return "userinfo"
 }
